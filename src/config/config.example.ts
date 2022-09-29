31a81fba9138851328646286fa092655f5280db7
JakubL <jakubxleszczewski@gmail.com>
2022-09-29 15:27:41 +0200

    project configuration

---
+++ src/config/config.example.ts
@@ -0,0 +1,14 @@
+export const config = {
+  itemsCountPerPage: 10,
+  dbHost: 'host',
+  dbPort: 1234,
+  dbDatabase: 'db_name',
+  dbUsername: 'db_username',
+  dbPassword: 'db_password',
+  dbSynchronize: false,
+  dbLogging: false,
+  jwtSecret: '',
+  jwtTimeToExpire: '0s',
+  jwtCookieTimeToExpire: 0,
+  jwtCookieDomain: 'host',
+};
