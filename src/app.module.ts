b08a6a69306af6d2f31213e70dfceba0b408e193
JakubL <jakubxleszczewski@gmail.com>
2022-09-30 17:23:38 +0200

    generate book module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { DatabaseModule } from './common/providers/database/database.module';
 import { UserModule } from './user/user.module';
 import { MailModule } from './common/providers/mail/mail.module';
 import { AuthModule } from './auth/auth.module';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [DatabaseModule, MailModule, UserModule, AuthModule],
+  imports: [DatabaseModule, MailModule, UserModule, AuthModule, BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
